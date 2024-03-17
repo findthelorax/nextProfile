697a09b36b17c86a29e977bdcb03bf9e8b3813dc
unknown <findthelorax@gmail.com>
2024-03-17 11:19:02 -0400

    styling, footer

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,17 +15,17 @@ export default function Navbar() {
 
     return (
         <div className="sticky top-0 z-50 backdrop-blur-sm p-4">
-            <nav className="flex items-center justify-between p-4 w-1/2 mx-auto">
+            <nav className="flex items-center justify-between p-4 w-3/4 mx-auto navbar">
                 <div>
                     <a href="/">
                         <img src="/path-to-your-logo.png" alt="Logo" className="h-8" />
                     </a>
                 </div>
                 <div className="space-x-4">
-                    <a href="/">Home</a>
-                    <a href="/profile">Projects</a>
-                    <a href="/about">About</a>
-                    <a href="/contact">Contact</a>
+                    <a href="/">HOME</a>
+                    <a href="/profile">PROJECTS</a>
+                    <a href="/about">ABOUT</a>
+                    <a href="/contact">CONTACT</a>
                     <button className="align-middle text-2xl" onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}>
                         {resolvedTheme === 'dark' ? <PiSunHorizonDuotone color="#FDB813" /> : <PiMoonStarsDuotone color="#4A4A68" />}
                     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 'use client'
 import Navbar from '@/components/NavBar';
+import Footer from '@/components/Footer';
 
 export default function Home() {
 
 	return (
-		<>
-			<main className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen">
+            <main className="flex-grow">
 			<Navbar />
 			</main>
-		</>
+			<Footer />
+		</div>
 	);
 }
