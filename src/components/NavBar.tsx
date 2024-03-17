6990dff38df0210c8c0f1c362f4e62a70b74d810
unknown <findthelorax@gmail.com>
2024-03-17 09:42:34 -0400

    light and dark button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { PiSunHorizonDuotone, PiMoonStarsDuotone } from "react-icons/pi";
+
 export default function Navbar() {
+    const { resolvedTheme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    // After mounting, we have access to the theme
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) {
+        return null;
+    }
+
     return (
         <div className="sticky top-0 z-50 backdrop-blur-sm p-4">
             <nav className="flex items-center justify-between p-4 w-1/2 mx-auto">
@@ -9,9 +23,12 @@ export default function Navbar() {
                 </div>
                 <div className="space-x-4">
                     <a href="/">Home</a>
-                    <a href="/profile">Profile</a>
+                    <a href="/profile">Projects</a>
                     <a href="/about">About</a>
                     <a href="/contact">Contact</a>
+                    <button className="align-middle text-2xl" onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}>
+                        {resolvedTheme === 'dark' ? <PiSunHorizonDuotone color="#FDB813" /> : <PiMoonStarsDuotone color="#4A4A68" />}
+                    </button>
                 </div>
             </nav>
         </div>
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Navbar from '@/components/NavBar';
+
+export default function Home() {
+
+	return (
+		<>
+			<main className="flex flex-col min-h-screen">
+			<Navbar />
+			</main>
+		</>
+	);
+}
